9bf3e650225eee6a127eb953649f9af322a10044
Antonio <antoniojosedonishung@gmail.com>
2023-06-10 09:45:33 -0500

    feat: new main that queries directly from the server

--- go-crawler/main.go
+++ go-crawler/main.go
@@ -1,37 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil" // Parse requests
 	"encoding/json" // Write json file
+	"fmt"           // Parse requests
 	"os"
-	//"bufio"
-	"time"
-	"regexp" // Regular expressions
-	"strings"
-	"bytes"
-	"strconv"
 
-	
-	"github.com/ericchiang/css"
-	"golang.org/x/net/html"
+	//"bufio"
+
+	// Regular expressions
+
+	"github.com/PedroChaparro/PI202202-alako-data/scraper"
+	"github.com/PedroChaparro/PI202202-alako-data/utils"
 )
 
 type Query struct {
-	Name string `json:"name"`	
+	Name string `json:"name"`
 	File string `json:"file"`
-	Cols int `json:"cols"`
+	Cols uint8  `json:"cols"`
 }
 
 type Hour struct {
 	Day   string `json:"day"`
-	Start int `json:"start"`
-	End   int `json:"end"`
+	Start int    `json:"start"`
+	End   int    `json:"end"`
 }
 
 type Course struct {
-	Ctg string `json:"ctg"`
-	Level string `json:"level"`
+	Ctg   string `json:"ctg"`
+	Level uint8  `json:"level"`
 	Title string `json:"title"`
 	Nrc   string `json:"nrc"`
 	Hours []Hour `json:"hour"`
@@ -55,233 +51,54 @@ var queries = []Query{
 	},
 }
 
-var courses []Course = []Course{}
-var pathWD string
-
-// TODO handle this case 12:00 PM-01:40 PM C203
-func formatHours(clnStr string) (int, int) {
-	fmt.Println("UUUUUUUUUUUUUUUU", clnStr, "UU")
-	words := strings.Split(clnStr, "-")
-
-	// first digit
-	firDig, _ := strconv.Atoi(strings.Split(words[0], ":")[0])
-	firAmpm := strings.Split(words[0], " ")[1]
-
-	// second
-	secDig, _ := strconv.Atoi(strings.Split(words[1], ":")[0])
-	secAmpm := strings.Split(words[1], " ")[1]
-
-	if (firAmpm == "PM" && firDig < 12){
-		firDig += 12
+func createNewHour(day string, hourStr string) Hour {
+	fmt.Println(day, hourStr)
+	newH := Hour{
+		Day:   "lun",
+		Start: 10,
+		End:   11,
 	}
-
-	if (secAmpm == "PM" && secDig < 12){
-		secDig += 12
-	}
-
-	secDig++
-
-	fmt.Println(firDig, firAmpm)
-	fmt.Println(secDig, secAmpm)
-	//return strconv.Itoa(firDig), strconv.Itoa(secDig)
-	return firDig, secDig
+	panic(1)
+	return newH
 }
 
-func createNewHour(day string, hourStr string) Hour{
-
-	// create new hours
-	newhstart, newhend := formatHours(hourStr)
-	newh := Hour{
-		Day  : day,
-		Start: newhstart,
-		End  : newhend,
-	}
-
-	fmt.Println(newh)
-	return newh
-}
-
-func getData(file string, cols int, ctg string) (err error) {
-
-	fmt.Printf("Current file: %s\n", file)
-
-	// ### ### ###
-	// Get plain html
-	// Open the file.
-	f, _ := os.Open(file)
-	defer f.Close()
-
-	b, err := ioutil.ReadFile(file)
-	if err != nil {
-		panic(err)
-	}
-
-	bodyStr := string(b)
-
-	// regex
-
-	regexTd, err := regexp.Compile("<[^>]+>([^<]+)")
-	if err != nil {
-		fmt.Println("Invalid regexp")
-		return // problem with the regular expression.
-	}
-
-	// css select
-
-	selectorStr := fmt.Sprintf("td:first-child:nth-last-child(%d) ~ td", cols)
-	fmt.Println(selectorStr)
-
-	sel, err := css.Parse(selectorStr)
-	if err != nil {
-		panic(err)
-	}
-	node, err := html.Parse(strings.NewReader(bodyStr))
-	if err != nil {
-		panic(err)
-	}
-
-	// filter lines
-
-	counter := 0
-	skip := true
-
-	offset := 0
-	if (cols > 12) {
-		offset = 1
-	}
-
-	// course properties
-	course := Course{
-		Level: "",
-		Title: "",
-		Nrc  : "",
-		Hours: []Hour{},
-	}
-
-	for _, ele := range sel.Select(node) {
-
-
-		var strBuff bytes.Buffer
-		html.Render(&strBuff, ele)
-
-		fmt.Println(counter -offset, "===", skip)
-
-		if (counter == 0){
-			//if (!skip){
-				//fmt.Println(course)
-			//}
-
-			skip = false
-			course = Course{
-				Level: "",
-				Title: "",
-				Nrc  : "",
-				Hours: []Hour{},
+func downloadData() []Course {
+	courses, err := scraper.Crawl()
+	utils.Fatal(err)
+	result := make([]Course, 0, len(courses))
+	for _, course := range courses {
+		hours := make([]Hour, 0, len(course.Hours))
+		for _, hour := range course.Hours {
+			h := Hour{
+				Day:   hour.Day.String(),
+				Start: hour.From.Hour(),
+				End:   hour.To.Hour(),
 			}
+			hours = append(hours, h)
 		}
-
-		if (!skip){
-
-			var clnStr string = ""
-			rawStr := strBuff.String()
-			submatch := regexTd.FindStringSubmatch(rawStr)
-			if (len(submatch) > 1){
-				clnStr = submatch[1]
-			}
-
-			fmt.Println(clnStr)
-
-			
-			switch (counter-offset) {
-
-				// title
-				case 0:
-
-					if (rawStr == "<td>ASIGNATURA</td>" || rawStr == "<td> </td>"){
-						skip = true
-					} else {
-						course.Title = clnStr
-					}
-
-				case -1:
-					
-					// it has levels
-					if (cols > 12) {
-						course.Level = clnStr
-					}
-
-				case 3:
-					course.Nrc = clnStr
-
-				// lunes
-				case 5:
-					if (strings.TrimSpace(clnStr) != ""){
-						course.Hours = append(course.Hours, createNewHour("lun", clnStr))
-					}
-				case 6:
-					if (strings.TrimSpace(clnStr) != ""){
-						course.Hours = append(course.Hours, createNewHour("mar", clnStr))
-					}
-				case 7:
-					if (strings.TrimSpace(clnStr) != ""){
-						course.Hours = append(course.Hours, createNewHour("mie", clnStr))
-					}
-				case 8:
-					if (strings.TrimSpace(clnStr) != ""){
-						course.Hours = append(course.Hours, createNewHour("jue", clnStr))
-					}
-				case 9:
-					if (strings.TrimSpace(clnStr) != ""){
-						course.Hours = append(course.Hours, createNewHour("vie", clnStr))
-					}
-				case 10:
-					if (strings.TrimSpace(clnStr) != ""){
-						course.Hours = append(course.Hours, createNewHour("sab", clnStr))
-					}
-					if (!skip){
-						course.Ctg = ctg
-						fmt.Println(course)
-						courses = append(courses, course)
-					}
-
-			}
-
-			//fmt.Println(str)
-
+		c := Course{
+			Ctg:   course.Faculty,
+			Level: course.Level,
+			Title: course.Subject,
+			Nrc:   course.NRC,
+			Hours: hours,
 		}
-
-		counter++
-		counter = counter % (cols -1)
+		result = append(result, c)
 	}
-
-
-	return nil
+	return result
 }
 
-func main(){
-
-	pathWD, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
-		return
+func main() {
+	output := os.Stdout
+	if len(os.Args) > 1 {
+		var err error
+		output, err = os.OpenFile(os.Args[1], os.O_WRONLY, 0777)
+		utils.Fatal(err)
 	}
-
-	// For each query
-	for _, query := range(queries) {
-
-		currLength := len(courses)
-		start := time.Now()
-		filePath := pathWD + "/datain/" + query.File
-
-		fmt.Printf("🏃 Starting with query: %s\n", query.File)
-		getData(filePath, query.Cols, query.Name)
-		fmt.Println(courses)
-
-		fmt.Printf("🏁 %d items were saved in %s\n\n", len(courses) - currLength, time.Since(start))
-	}
-
-	// Create json file
-	jsonString, _ := json.MarshalIndent(courses, "", "	")
-	ioutil.WriteFile("dataout/data.json", jsonString, os.ModePerm)
+	courses := downloadData()
+	encoder := json.NewEncoder(output)
+	encoder.SetIndent("", "\t")
+	err := encoder.Encode(courses)
+	utils.Fatal(err)
 
 }
